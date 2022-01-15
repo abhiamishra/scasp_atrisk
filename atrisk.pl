6062377c35c572a610e6c000dff83f740c4dc06f
Abhishek A. Mishra <52009380+abhiamishra@users.noreply.github.com>
2022-01-15 12:33:22 -0600

    commiting

--- atrisk.pl
+++ atrisk.pl
@@ -1,23 +1,5 @@
-%%define what an immediate risk student is
-immediate_risk(X) :- haveChild(X).
-immediate_risk(X) :- onParole(X).
-immediate_risk(X) :- isGovCare(X).
-immediate_risk(X) :- homeless(X).
-immediate_risk(X) :- abused(X).
 
-%%define what a goodEnglishLearner is
-goodEnglishLearner(X) :- englishPass(X), esl(X).
-
-%%define what an academically good student is
-academicallyGood(X) :- not englishPass(X), sciencePass(X), historyPass(X), mathPass(X).
-academicallyGood(X) :- englishPass(X), not sciencePass(X), historyPass(X), mathPass(X).
-academicallyGood(X) :- englishPass(X), sciencePass(X), not historyPass(X), mathPass(X).
-academicallyGood(X) :- englishPass(X), sciencePass(X), historyPass(X), not mathPass(X).
-academicallyGood(X) :- englishPass(X), sciencePass(X), historyPass(X), mathPass(X).
-
-poorSchoolPast(X) :- hadDrop(X), not advanced(X).
-poorSchoolPast(X) :- not -isBullied(X), not advanced(X).
-poorSchoolPast(X) :- not -isBullied(X), hadDrop(X).
+%% GRADES AND ACADEMICS %%
 
 %%How to pass the grade
 sciencePass(X) :- scienceGrade(X,A), A>=70.
@@ -25,37 +7,61 @@ mathPass(X) :- mathGrade(X,A), A>=70.
 englishPass(X) :- englishGrade(X,A), A>=70.
 historyPass(X) :- historyGrade(X,A), A>=70.
 
-%%Find if the student is a STEM student
-isStem(X) :- sciencePass(X), mathPass(X), scienceGrade(X,A), mathGrade(X,B), A+B>=170.
+%%define academicallyGood student as student pass all classes with 70
+academicallyGood(X) :- not englishPass(X), sciencePass(X), historyPass(X), mathPass(X).
+academicallyGood(X) :- englishPass(X), not sciencePass(X), historyPass(X), mathPass(X).
+academicallyGood(X) :- englishPass(X), sciencePass(X), not historyPass(X), mathPass(X).
+academicallyGood(X) :- englishPass(X), sciencePass(X), historyPass(X), not mathPass(X).
+academicallyGood(X) :- englishPass(X), sciencePass(X), historyPass(X), mathPass(X).
+
+%%Find if the student is a special student (category)
+isStem(X) :- scienceGrade(X,A), mathGrade(X,B), A+B>=170.
 isSocSci(X) :- englishPass(X), historyPass(X), englishGrade(X,A), historyGrade(X,B), A+B>=170.
 isSocSci(X) :- esl(X), englishGrade(X,A), historyGrade(X,B), A+B>=150.
 isHonorRoll(X) :- scienceGrade(X,A), mathGrade(X,B), englishGrade(X,C), historyGrade(X,D), (A+B+C+D)/4>=90.
 
-%%Teacher-observed behavior
-findBehavior(X, [X|_]).
-findBehavior(X, [_|T]) :- findBehavior(X, T).
-immediate_risk(X) :- behavior(X), findBehavior(X,['violent','depressed','lonely','repressed','abused','harassed']).
-goodBehaviorWithOther(X) :- behavior(X), not findBehavior(X, ['distant', 'frustrated', 'agitated', 'alone', 'fearful', 'noncooperation', 'impulsive']).
+%%define what a goodEnglishLearner is
+goodEnglishLearner(X) :- englishPass(X), esl(X).
 
+%% GRADES AND ACADEMICS %%
 
-motivatedStudent(X) :-
-    goodAttendance(X),
-    academiclyGood(X).
+%% BEHAVIORS %%
 
-motivatedStudent(X) :- academicallyGood(X), goodBehaviorWithOther(X).
+%%define student with positive and negative emotion based on what we have
+goodEmotion(X) :-  not badSchoolPast(X), academicallyGood(X).
+goodEmotion(X) :- not badSchoolPast(X), goodAttendance(X).
 
+%%define student that have good behavior with other 
+goodBehaviorWithOther(X) :- goodEmotion(X), -esl(X).
+goodBehaviorWithOther(X) :- goodEmotion(X), isStem(X).
+goodBehaviorWithOther(X) :- goodEmotion(X), isSocSci(X).
+goodBehaviorWithOther(X) :- goodEmotion(X), goodEnglishLearner(X).
+-goodBehaviorWithOther(X) :- esl(X), not goodEnglishLearner(X).
+goodBehaviorWithOther(X) :- goodEmotion(X), badSchoolPast(X).
 
-at_risk(X) :- immediate_risk(X), not motivatedStudent(X).
-at_risk(X) :- not motivatedStudent(X), poorSchoolPast(X).
--at_risk(X) :- -immediate_risk(X).
--at_risk(X) :- academiclyGood(X),motivatedStudent(X).
--at_risk(X) :- -poorSchoolPast(X).
+motivatedStudent(X) :- goodEnglishLearner(X), academicallyGood(X).
+motivatedStudent(X) :- goodBehaviorWithOther(X), goodEmotion(X), not -academicallyGood(X).
+motivatedStudent(X) :- goodAttendance(X), academicallyGood(X), isStem(X).
+motivatedStudent(X) :- goodAttendance(X), academicallyGood(X), isSocSci(X).
+-motivatedStudent(X) :- not goodAttendance(X), badSchoolPast(X).
 
+%% BEHAVIORS %%
 
-englishPass(Dan).
--sciencePass(Dan).
-historyPass(Dan).
-mathPass(Dan).
-goodAttendance(Dan).
+%% AT RISK CALCULATION %%
+
+at_risk(X) :- immediate_risk(X).
+at_risk(X) :- badSchoolPast(X), not goodBehaviorWithOther(X).
+
+-at_risk(X) :- motivatedStudent(X).
+-at_risk(X) :- isHonorRoll(X).
+-at_risk(X) :- goodBehaviorWithOther(X), goodAttendance(X).
+
+historyGrade(dan,90).
+scienceGrade(dan,90).
+mathGrade(dan,90).
+englishGrade(dan,90).
+esl(dan).
+goodAttendance(dan).
+badSchoolPast(dan).
 
 ?- at_risk(X).
