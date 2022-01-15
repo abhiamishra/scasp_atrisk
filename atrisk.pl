16323dcc63192f34c7a014293906158afdb9b4c6
Abhishek A. Mishra <52009380+abhiamishra@users.noreply.github.com>
2022-01-14 22:19:15 -0600

    new updates

--- atrisk.pl
+++ atrisk.pl
@@ -1,25 +1,23 @@
-immediate_risk(X) :- haveChild(X) , onParole(X) , isGovCare(X) , homeless(X) , abused(X).
+%%define what an immediate risk student is
+immediate_risk(X) :- haveChild(X).
+immediate_risk(X) :- onParole(X).
+immediate_risk(X) :- isGovCare(X).
+immediate_risk(X) :- homeless(X).
+immediate_risk(X) :- abused(X).
 
-goodEnglishLearner(X) :-
-    englishPass(X),
-    esl(X).
+%%define what a goodEnglishLearner is
+goodEnglishLearner(X) :- englishPass(X), esl(X).
 
-academiclyGood(X) :-
-    englishPass(X), 
-    sciencePass(X), 
-    historyPass(X), 
-    mathPass(X).
+%%define what an academically good student is
+academicallyGood(X) :- not englishPass(X), sciencePass(X), historyPass(X), mathPass(X).
+academicallyGood(X) :- englishPass(X), not sciencePass(X), historyPass(X), mathPass(X).
+academicallyGood(X) :- englishPass(X), sciencePass(X), not historyPass(X), mathPass(X).
+academicallyGood(X) :- englishPass(X), sciencePass(X), historyPass(X), not mathPass(X).
+academicallyGood(X) :- englishPass(X), sciencePass(X), historyPass(X), mathPass(X).
 
-academiclyGood(X) :-
-    englishPass(X), 
-    sciencePass(X), 
-    historyPass(X), 
-    mathPass(X).
-
-poorSchoolPast(X) :-
-    not -isBullied(X),
-    not -hadDrop(X),
-    not advanced(X).
+poorSchoolPast(X) :- hadDrop(X), not advanced(X).
+poorSchoolPast(X) :- not -isBullied(X), not advanced(X).
+poorSchoolPast(X) :- not -isBullied(X), hadDrop(X).
 
 motivatedStudent(X) :-
     goodAttendance(X),
@@ -33,10 +31,9 @@ at_risk(X) :- not motivatedStudent(X), poorSchoolPast(X).
 
 
 englishPass(Dan).
-sciencePass(Dan).
+-sciencePass(Dan).
 historyPass(Dan).
 mathPass(Dan).
-
 goodAttendance(Dan).
 
 ?- at_risk(X).
